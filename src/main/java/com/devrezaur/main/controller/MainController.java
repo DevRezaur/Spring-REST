07f2c9ec88eee807220ed5e12a019eae2e41608e
Rezaur Rahman <rezaur.official@gmail.com>
2021-05-13 15:13:02 +0600

    Added: Update & Delete

--- src/main/java/com/devrezaur/main/controller/MainController.java
+++ src/main/java/com/devrezaur/main/controller/MainController.java
@@ -15,45 +15,66 @@ import com.devrezaur.main.repository.StudentRepository;
 
 @RestController
 public class MainController {
-	
+
 	@Autowired
 	private StudentRepository studentRepo;
 	@Autowired
 	private CourseRepository courseRepo;
-	
+
 	@GetMapping("/")
 	public String hello() {
 		return "Hello coders...";
 	}
-	
+
 	@GetMapping("/student/all")
 	public List<Student> getStudents() {
 		return studentRepo.findAll();
 	}
-	
+
 	@GetMapping("/student/{id}")
 	public Optional<Student> getStudent(@PathVariable("id") int id) {
 		return studentRepo.findById(id);
 	}
-	
+
 	@GetMapping("/course/all")
 	public List<Course> getCourses() {
 		return courseRepo.findAll();
 	}
-	
+
 	@GetMapping("/course/{id}")
 	public Optional<Course> getCourse(@PathVariable("id") int id) {
 		return courseRepo.findById(id);
 	}
-	
+
 	@PostMapping("/student/add")
 	public Student addStudent(@RequestBody Student student) {
 		return studentRepo.save(student);
 	}
-	
+
 	@PostMapping("/course/add")
 	public Course addCourse(@RequestBody Course course) {
 		return courseRepo.save(course);
 	}
-	
+
+	@PostMapping("/student/update")
+	public Student updateStudent(@RequestBody Student student) {
+		return studentRepo.save(student);
+	}
+
+	@GetMapping("/student/{id}/delete")
+	public String deleteStudent(@PathVariable("id") int id) {
+		try {
+			studentRepo.deleteById(id);
+		} catch (Exception ex) {
+			return "Failed";
+		}
+
+		return "Successful";
+	}
+
+	@PostMapping("/course/update")
+	public Course updateCourse(@RequestBody Course course) {
+		return courseRepo.save(course);
+	}
+
 }
